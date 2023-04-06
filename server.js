9afe4d7bb39125d57d26c4f8389d21711cea9c51
Non <chanon430922@gmail.com>
2023-04-06 10:24:06 +0700

    add API security

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -21,6 +27,28 @@ app.use(express.json());
 
 //Cookie parser
 app.use(cookieParser());
+
+//Sanitize Data
+app.use(mongoSanitize());
+
+//Set security headers
+app.use(helmet());
+
+//prevent XSS attacks
+app.use(xss());
+
+//Rate Limitting
+const limiter = rateLimit({
+    windowsMS:10*60*100, //10 mins
+    max: 5
+});
+app.use(limiter);
+
+//Prevent http param pollutions
+app.use(hpp());
+
+//Enable CORS
+app.use(cors());
 
 app.use('/api/v1/hospitals', hospitals);
 app.use('/api/v1/auth', auth);

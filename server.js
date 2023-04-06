ab39723b2820ce9fe29b876d6cfbff4a53d4588b
Non <chanon430922@gmail.com>
2023-04-06 11:51:30 +0700

    Creating OpenAPI Document with Swagger

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
-const cors = require('cors');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
 const hpp = require('hpp');
 const cors = require('cors');
+
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -20,6 +22,27 @@ const auth = require('./routes/auth');
 const appointments = require('./routes/appointments');
 
 const app=express();
+
+const swaggerOptions = {
+    swaggerDefinition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Library API',
+        version: '1.0.0',
+        description: 'A simple Express VacQ API'
+      },
+      servers: [
+        {
+            url: 'http://localhost:5000/api/v1'
+        }
+      ],
+    },
+    apis: ['./routes/*.js']
+};
+  
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));  
+
 app.use(cors());
 
 //Body parser
